231663a7e22216b5d8beb30672cc890c0764a2af
wsq03 <zui031316@163.com>
2016-12-01 17:20:18 +0800

    第一次

---
+++ app/src/main/java/com/feicuiedu/gitdroid/MainActivity.java
@@ -0,0 +1,121 @@
+package com.feicuiedu.gitdroid;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.feicuiedu.gitdroid.login.LoginActivity;
+
+import junit.framework.Protectable;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private Context mContext;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.navigationView)
+    NavigationView mNavigationView;
+    @BindView(R.id.drawerLayout)
+    DrawerLayout mDrawerLayout;
+    private Button mBtnLogin;
+    private ImageView mIvIcon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = this;
+        // 设置当前视图(更改了当前视图内容,将导致onContentChanged方法触发)
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+        ButterKnife.bind(this);
+
+        /**
+         * 需要处理的视图
+         * 1. toolbar
+         * 2. DrawerLayout
+         * 3. NavigationView
+         */
+        //设置Action
+        setSupportActionBar(mToolbar);
+        // 设置监听
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.syncState();
+
+        //设置DrawerLayout的监听
+        mDrawerLayout.addDrawerListener(toggle);
+
+        //mNavigationView中子条目设置监听事件
+        mNavigationView.setNavigationItemSelectedListener(this);
+
+        mBtnLogin = ButterKnife.findById(mNavigationView.getHeaderView(0), R.id.btnLogin);
+        mIvIcon = ButterKnife.findById(mNavigationView.getHeaderView(0), R.id.ivIcon);
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: 2016/12/1 0001 跳转到登录界面
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+
+    }
+
+
+    //主要做了我们基本登录信息的改变
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // TODO: 2016/12/1 0001
+        if (item.isChecked()) {
+            item.setCheckable(false);
+        }
+        switch (item.getItemId()) {
+            //最热门
+            case R.id.github_hot_repo:
+                break;
+            //开发者
+            case R.id.github_hot_coder:
+                break;
+            //我的收藏
+            case R.id.arsenal_my_repo:
+                break;
+            //每日干货
+            case R.id.tips_daily:
+                break;
+        }
+        //选择一项之后，切换Fragment,关闭抽屉
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            //  mDrawerLayout.
+        }
+    }
+}
